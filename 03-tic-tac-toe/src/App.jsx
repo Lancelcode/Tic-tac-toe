c5837c678ca747dc7154e7b74614a0004d44c19f
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2023-06-10 16:08:19 +0100

    tictactoe

---
+++ 03-tic-tac-toe/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+const TURNS = {
+  x: 'x',
+  o: 'o'
+}
+
+const Square = ({children, updateBoard, index})=>{
+  return(
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.x)
+
+  return (
+  <main className='board'>
+  <h1>Tic tac toe</h1>
+  
+  <section className="game">
+    {
+      board.map((_, index)=>{
+        return (
+          <Square
+            key={index}
+            index={index}
+            >
+              {index}
+            </Square>
+        )
+      })
+    }
+  </section>
+
+  <section className="turn">
+    <Square isSelected={turn===TURNS.x}>
+      {TURNS.x}
+      </Square>
+    <Square isSelected={turn===TURNS.o}>
+      {TURNS.o}
+      </Square>
+  </section>
+  </main>
+  )
+}
+
+
+export default App

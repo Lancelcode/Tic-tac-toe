efe9b1290fa645626e4a4b9eecf05b0f87c9e9a6
Lancelcode <sowrebollo@gmail.com>
2023-06-29 21:07:53 +0100

    checkWinnerFrom logic

---
+++ src/logic/board.js
@@ -0,0 +1,18 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    //Revisamos todas las convinaciones ganadoras
+    //para ver si x u o gano.
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+          return boardToCheck[a]
+      }
+    }
+    //si no hay ganador.
+    return null
+}
